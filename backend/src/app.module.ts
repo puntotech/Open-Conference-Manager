2d16c3c201e7a1c8ab5cd0c7d4358a3ca76e9c3e
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-09-16 14:06:59 +0200

    feat(app.module): add TalkModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
-import { SpeakerModule } from 'src/domain/speaker/speaker.module';
-import { Module } from '@nestjs/common';
 import { AuthModule } from '@modules/auth/auth.module';
+import { Module } from '@nestjs/common';
+import { SpeakerModule } from 'src/domain/speaker/speaker.module';
+import { TalkModule } from '@modules/talk/talk.module';
 
 @Module({
-  imports: [SpeakerModule, AuthModule],
+  imports: [SpeakerModule, AuthModule, TalkModule],
   controllers: [],
   providers: [],
 })
