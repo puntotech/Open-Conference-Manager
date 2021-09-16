9497453638ca9d75cd8a551bade9c8c8b20ebfba
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-09-16 14:09:48 +0200

    fix(talk.controller): ParseIntPipe

--- backend/src/domain/talk/talk.controller.ts
+++ backend/src/domain/talk/talk.controller.ts
@@ -15,24 +15,19 @@ import { TalkService } from './talk.service';
 import { Talk } from './talk.entity';
 import { AuthGuard } from '@guards/auth.guard';
 
-@Controller('talk')
+@Controller('talks')
 //@UseGuards(AuthGuard)
 export class TalkController {
   constructor(private readonly talkService: TalkService) {}
 
   @Get('/:id')
-  getByID(@Param(new ParseIntPipe()) id: number): Promise<Talk> {
+  getByID(@Param('id', ParseIntPipe) id: number): Promise<Talk> {
     return this.talkService.getByID(id);
   }
 
   @Get()
   getTalks(): Promise<Talk[]> {
     return this.talkService.getAll();
-  }
-
-  @Get('/speaker/:id')
-  getSpeakerTalks(@Param('id') id: string): Promise<Talk[]> {
-    return this.talkService.getBySpeaker(id);
   }
 
   //TODO: add validation dto
