60e60040cd818e383bcc0d4304318ce797c7aae1
nyagarcia <nyablk97@gmail.com>
2021-09-17 23:01:14 +0200

    feat(auth.module): change jwt options and export authService

--- backend/src/domain/auth/auth.module.ts
+++ backend/src/domain/auth/auth.module.ts
@@ -2,13 +2,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleAuthService } from './google-auth-service';
 import { JwtModule } from '@nestjs/jwt';
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { SpeakerModule } from '@modules/speaker/speaker.module';
+import { jwtConstants } from 'jwt-constants';
 
+@Global()
 @Module({
-  imports: [JwtModule.register(null), SpeakerModule],
+  imports: [
+    SpeakerModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, GoogleAuthService],
-  exports: [],
+  exports: [AuthService],
 })
 export class AuthModule {}
