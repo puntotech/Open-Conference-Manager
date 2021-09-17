2805bfdd282ce347751f6fb80b8a4a92b91a03e4
nyagarcia <nyablk97@gmail.com>
2021-09-17 21:52:05 +0200

    feat(auth-guard.service): update guard

--- frontend/src/app/shared/guards/auth-guard.service.ts
+++ frontend/src/app/shared/guards/auth-guard.service.ts
@@ -4,45 +4,32 @@ import {
   Router,
   RouterStateSnapshot,
 } from "@angular/router";
-import { filter, map, tap } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { SocialAuthService } from "angularx-social-login";
 import { UserService } from "src/app/user/services/user.service";
+import { routes } from "src/app/consts/routes";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private authService: SocialAuthService, private router: Router) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   canActivate(
     _: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | Observable<boolean> | Promise<boolean> {
-    console.log("AuthGuard#canActivate called");
-
-    return this.authService.authState.pipe(
+    return this.userService.user$.pipe(
       map((user) => !!user),
       tap((authenticated) => {
         if (!authenticated) {
-          this.router.navigate;
+          this.router.navigate([routes.LOGIN], {
+            queryParams: { returnUrl: state.url },
+          });
         }
       })
     );
-
-    /* if (this.authService.authState) {
-      console.log(this.authService.authState);
-      return true;
-    }
-
-    console.log("AuthGuard#canActivate not authorized to access page");
-
-    this.router.navigate(["/user/login"], {
-      queryParams: { returnUrl: state.url },
-    });
-
-    return false; */
   }
 }
