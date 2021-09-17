cf171c1f06b8cfd6676f34f2e10faee85a18c3a2
nyagarcia <nyablk97@gmail.com>
2021-09-17 22:46:47 +0200

    feat(speaker.service): add upsert method

--- backend/src/domain/speaker/speaker.service.ts
+++ backend/src/domain/speaker/speaker.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Speaker } from './speaker.entity';
 import { SpeakerRepository } from './speaker.repository';
+import { FindManyOptions, FindOneOptions } from 'typeorm';
 
 @Injectable()
 export class SpeakerService {
@@ -14,10 +15,12 @@ export class SpeakerService {
       .andWhere('talk.status=1')
       .getOne();
   }
-  public find(options): Promise<Speaker[]> {
+
+  public find(options?: FindManyOptions<Speaker>): Promise<Speaker[]> {
     return this.speakerRepository.find(options);
   }
-  public findOne(options): Promise<Speaker> {
+
+  public findOne(options?: FindOneOptions<Speaker>): Promise<Speaker> {
     return this.speakerRepository.findOne(options);
   }
 
@@ -36,5 +39,15 @@ export class SpeakerService {
   public async update(speaker: Partial<Speaker>): Promise<Speaker> {
     const oldSpeaker = await this.findByID(speaker.id);
     return Object.assign(oldSpeaker, speaker).save();
+  }
+
+  public async upsert(speaker: Partial<Speaker>) {
+    const oldSpeaker = await this.speakerRepository.findOne({
+      email: speaker.email,
+    });
+    if (oldSpeaker) {
+      return Object.assign(oldSpeaker, speaker).save();
+    }
+    return this.create(speaker);
   }
 }
