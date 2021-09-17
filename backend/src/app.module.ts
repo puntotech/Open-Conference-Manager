26c86629c433450e3073f4e1e19aac00e132336c
nyagarcia <nyablk97@gmail.com>
2021-09-17 23:03:20 +0200

    feat(auth.module): remove middleware configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,12 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import { AuthMiddleware } from './shared/middleware/auth.middleware';
 import { AuthModule } from '@modules/auth/auth.module';
 import { SpeakerModule } from 'src/domain/speaker/speaker.module';
 import { TalkModule } from '@modules/talk/talk.module';
 
 @Module({
-  imports: [SpeakerModule, AuthModule, TalkModule],
+  imports: [AuthModule, SpeakerModule, TalkModule],
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('talks', 'speakers');
-  }
-}
+export class AppModule {}
