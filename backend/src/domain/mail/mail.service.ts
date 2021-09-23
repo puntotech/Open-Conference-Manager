d26c355a9775f6cadd7dc9f1cb6415eaead962be
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-09-23 12:57:29 +0200

    feat(mail): update new-submission template

--- backend/src/domain/mail/mail.service.ts
+++ backend/src/domain/mail/mail.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Speaker } from '@modules/speaker/speaker.entity';
 import { MailerService } from '@nestjs-modules/mailer';
+import { Speaker } from '@modules/speaker/speaker.entity';
 import { Talk } from '@modules/talk/talk.entity';
 
 @Injectable()
@@ -8,32 +8,43 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendSubmittedTalkConfirmation(talk: Talk, speaker: Speaker) {
-    await this.mailerService.sendMail({
-      to: speaker.email,
-      // from: '"Support Team" <support@example.com>', // override default from
-      subject: 'Submitted talk to BiznagaFest',
-      template: './talk-confirmation',
-      context: {
-        name: speaker.name,
-        title: talk.title,
-      },
-    });
+    await this.mailerService
+      .sendMail({
+        to: speaker.email,
+        // from: '"Support Team" <support@example.com>', // override default from
+        subject: 'Submitted talk to BiznagaFest',
+        template: './talk-confirmation',
+        context: {
+          name: speaker.name,
+          title: talk.title,
+        },
+      })
+      .catch(console.log);
   }
 
   async sendSubmissionNotification(talk: Talk, speaker: Speaker) {
-    await this.mailerService.sendMail({
-      to: 'nyablk97@gmail.com',
-      // from: '"Support Team" <support@example.com>', // override default from
-      subject: 'Submitted talk to BiznagaFest',
-      template: './new-submission',
-      context: {
-        name: speaker.name,
-        title: talk.title,
-        abstract: talk.abstract,
-        language: talk.language,
-        level: talk.level,
-        comments: talk.comments,
-      },
-    });
+    const coSpeakers = talk.speakerTalkStatus.slice(1);
+    const coSpeakerNames = coSpeakers.reduce(
+      (prev, curr) => `${prev} ${curr.speaker.name},`,
+      '',
+    );
+
+    await this.mailerService
+      .sendMail({
+        to: 'nyablk97@gmail.com',
+        // from: '"Support Team" <support@example.com>', // override default from
+        subject: 'Submitted talk to BiznagaFest',
+        template: './new-submission',
+        context: {
+          name: speaker.name,
+          title: talk.title,
+          abstract: talk.abstract,
+          language: talk.language,
+          level: talk.level,
+          comments: talk.comments,
+          coSpeakerNames,
+        },
+      })
+      .catch(console.log);
   }
 }
