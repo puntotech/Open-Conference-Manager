71baedd00025784e0aeb0abc5036d0ce8c3b9077
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2021-10-01 22:19:26 +0200

    fix(main): enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,8 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
