571727bb146e09a3653f19221d2bf5ef2eda1e87
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2021-10-01 21:09:37 +0200

    feat(build): environment.prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  APIENDPOINT_BACKEND: "https://ocm.api.gdgmalaga.dev",
 };
+
+
+export const GOOGLE_KEY =
+  "703636158022-ck52l71r9bh3cttfn9fap9ec9pabj599.apps.googleusercontent.com";
+
+export const FACEBOOK_KEY = "1219706015189968";
