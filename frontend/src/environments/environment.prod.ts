4a6f0c9204b0cbf229afd2897e79cf5989baedcf
juanT <imzaldih@gmail.com>
2021-10-01 11:42:33 +0200

    chore(front): add production config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  APIENDPOINT_BACKEND: "https://ocm.api.gdgmalaga.dev",
 };
+
+export const GOOGLE_KEY =
+  "527493599469-f16jl5v1crcotl2pacojqp7s7lfk1dr6.apps.googleusercontent.com";
