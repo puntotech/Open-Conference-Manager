c757db5237c3e54dfcf1f41fab64d861a14e4f0b
Nya Garcia <nyablk97@gmail.com>
2021-10-03 16:52:57 +0200

    feat(frontend): add facebook data deletion instructions page

---
+++ frontend/src/app/about/pages/facebook-delete-data/facebook-delete-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facebook-delete-data',
+  templateUrl: './facebook-delete-data.component.html',
+  styleUrls: ['./facebook-delete-data.component.css']
+})
+export class FacebookDeleteDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
